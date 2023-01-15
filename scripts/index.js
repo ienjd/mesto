9c395c7b936f7347bc915ae07c64362846fa180a
Vlad Shutov <coldhobbit31@ya.ru>
2023-01-15 19:49:13 +0300

    feat: добавлен попап, свёрстано окно

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,15 @@
 const editButton = document.querySelector('.profile__edit-button-shell');
 const aboutPopup = document.querySelector('.popup');
 const popupClosed = document.querySelector('.popup__close-icon');
+const addCardButton = document.querySelector('.profile__add-button-shell');
+const addCardPopup = document.querySelector('.popup-add-card');
+const addCardPopupClosed = document.querySelector('.popup-add-card__close-icon');
 const formElement = document.querySelector('.popup__input');
-const nameUser = formElement.querySelector('.popup__input-text_type_name');
-const professionUser = formElement.querySelector('.popup__input-text_type_profession');
+const nameUser = formElement.querySelector('#type-name');
+const professionUser = formElement.querySelector('#type-profession');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
+
 
 function openForm() {
     nameUser.value = profileName.textContent;
@@ -18,6 +22,18 @@ function closeForm () {
     aboutPopup.classList.remove('popup_opened');
 };
 popupClosed.addEventListener('click',closeForm);
+
+function cardOpenForm() {
+    nameUser.value = profileName.textContent;
+    professionUser.value = profileProfession.textContent;
+    addCardPopup.classList.add('popup-add-card_opened');
+};
+addCardButton.addEventListener('click',cardOpenForm);
+
+function cardCloseForm () {
+    addCardPopup.classList.remove('popup-add-card_opened');
+};
+addCardPopupClosed.addEventListener('click',cardCloseForm);
 
 function formSubmit (evt) {
     evt.preventDefault();
